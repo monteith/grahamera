59fed88162a8d7070982ad2d8866706527bb1b31
monteith <monteith.g@gmail.com>
2017-08-26 15:46:39 -0500

    Add payload post/get for git webhook

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 let express = require('express');
+let bodyParser = require('body-parser');
+let exec = require('child_process').exec;
+
 let app = express();
 
+const prj_dir = '/var/www/pi/grahamera';
+
+const exec_callback = (err, stdout, stderr) => {
+  if(stdout) { console.log(stdout) };
+  if(stderr) console.log(stderr);
+};
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-
   res.send('Hello');
+});
 
+app.post('/payload', (req, res) => {
+  console.log(`${req.body.pusher.name} just pushed to ${req.body.repository.name}`);
+  console.log('pulling code from GitHub...');
+
+  // reset any local changes
+  exec(`git -C ${prj_dir} reset --hard`, exec_callback);
+
+  // ditch any files that were added locally
+  exec(`git -C ${prj_dir} clean -df`, exec_callback);
+
+  // pull latest code
+  exec(`git -C ${prj_dir} pull -f`, exec_callback);
+
+  // npm install
+  exec(`npm -C ${prj_dir} install --production`, exec_callback);
 });
 
 
