d320b69d0c0b0a47c06922ce6f14a2f966ebaa29
monteith <monteith.g@gmail.com>
2017-08-26 15:02:35 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let express = require('express');
+let app = express();
+
+app.get('/', (req, res) => {
+
+  res.send('Hello');
+
+});
+
+
+app.listen(5000, () => {
+  console.log(' === ', 'Listening on port 5000');
+});
